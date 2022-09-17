f34c0ab433aeb6f17c07725622bd66d820851806
Gökhan UTAR <g.utar308@gmail.com>
2022-09-17 18:37:02 +0300

    Initial commit

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 12;
+        String mesaj = "Öğrenci sayısı";
+
+        System.out.println(mesaj + sayi); 
+
+	}
+
+}
